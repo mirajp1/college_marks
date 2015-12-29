e224c629f3d289aea6f04c8d892a7b99e780dc39
mirajp1 <mirajp1@gmail.com>
2015-12-29 11:15:35 +0530

    set routes constraints for api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
 	#API definition
 	namespace :api,defaults: {format: :json},constraints: {subdomain: 'api'},path: '/' do
+		
+		#this is for versioning of api
+		scope module: :v1 do
+			#list resources here
+		end	
 
 	end
 
